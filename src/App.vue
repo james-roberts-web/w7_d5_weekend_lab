a4beb9ae46425a011bcc95fadcefa89a183e97ca
james-roberts-web <jamesroberts@xponorth.co.uk>
2019-10-19 15:50:13 +0100

    api read commit

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,30 @@
 </template>
 
 <script>
+
+import { eventBus } from '@/main.js';
+
 export default {
   name: 'app',
-}
 
+  data() {
+    return {
+      rockets: []
+    };
+  },
+
+  mounted() {
+    this.getRockets();
+  },
+
+  methods: {
+    getRockets() {
+      fetch('https://api.spacexdata.com/v3/rockets')
+      .then(result => result.json())
+      .then(data => this.rockets = data);
+    }
+  }
+}
 </script>
 
 <style lang="css" scoped>
