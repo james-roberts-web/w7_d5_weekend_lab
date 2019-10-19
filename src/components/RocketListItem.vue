bad0729e309277db8599be1ceaa61056024aa348
james-roberts-web <jamesroberts@xponorth.co.uk>
2019-10-19 16:11:53 +0100

    rocket list

---
+++ src/components/RocketListItem.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <p>{{rocket.rocket_name}}</p>
+</template>
+
+<script>
+
+import { eventBus } from '@/main.js';
+
+export default {
+  name: "rocket-list-item",
+  props: ["rocket"]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
